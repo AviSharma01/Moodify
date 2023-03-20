7867dd6af5d401fefcdea0b5d417c3b2327324c8
AviSharma01 <avisharma1204@gmail.com>
2023-03-19 21:21:29 -0700

    Updated remainder file

--- utils/reminder.py
+++ utils/reminder.py
@@ -1,11 +1,37 @@
 import datetime
 import subprocess
+import boto3
+import os
 
-def add_weekly_reminder(reminder_name, reminder_date):
-    reminder_date = datetime.datetime.strptime(reminder_date, "%m/%d/%Y")
-    today = datetime.datetime.now().date()
-    next_reminder_date = reminder_date + datetime.timedelta(weeks=(today - reminder_date.date()).days // 7)
-    next_reminder_date_str = datetime.datetime.strftime(next_reminder_date, "%Y-%m-%dT%H:%M:%SZ")
+def set_reminder(apple_id, reminder_name, reminder_date):
+    # Set reminder in Reminders app
+    reminder_date = datetime.datetime.strptime(reminder_date, "%Y-%m-%d %H:%M:%S")
     reminder_text = f"Reminder: {reminder_name} is available!"
-    applescript = f'do shell script "echo \'{reminder_text}\' | pbcopy && echo \'{reminder_text}\' | pbpaste | osascript -e \'tell application \\"Reminders\\" to make new reminder with properties {{name:\'{reminder_name}\', remind me date:date \\"{next_reminder_date_str}\\"}}\'"'
+    applescript = f'do shell script "echo \'{reminder_text}\' | pbcopy && echo \'{reminder_text}\' | pbpaste | osascript -e \'tell application \\"Reminders\\" to make new reminder with properties {{name:\'{reminder_name}\', remind me date:date \\"{reminder_date}\\"}}\'"'
     subprocess.call(["osascript", "-e", applescript])
+    
+    # Send email notification
+    subject = "Weekly playlist is available!"
+    body = f"The '{reminder_name}' playlist is now available on Spotify."
+    
+    client = boto3.client('ses', region_name=os.environ['AWS_REGION'], aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    response = client.send_email(
+        Destination={
+            'ToAddresses': [
+                os.environ['RECIPIENT_EMAIL']
+            ]
+        },
+        Message={
+            'Body': {
+                'Text': {
+                    'Charset': 'UTF-8',
+                    'Data': body,
+                },
+            },
+            'Subject': {
+                'Charset': 'UTF-8',
+                'Data': subject,
+            },
+        },
+        Source=os.environ['SENDER_EMAIL']
+    )
