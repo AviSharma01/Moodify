370e7e678d129fe01a3d2350c009eb7dc36f834f
AviSharma01 <avisharma1204@gmail.com>
2023-03-19 20:09:08 -0700

    Added the utils component plus made changes

---
+++ config/__init__.py
@@ -0,0 +1,1 @@
+# This file is empty, but it is included to mark the config directory as a Python package.
---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+# This file is empty, but it is included to mark the utils directory as a Python package.
